0720a7c7332acd5e6bf89683d9063851168777bc
sonnyresetti <sonnyresetti9@gmail.com>
2024-10-06 17:42:48 -0500

    Update app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1155725790"
+    serviceWorkerVersion: "1445427539"
   }
 });
